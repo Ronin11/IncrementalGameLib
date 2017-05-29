c9c1ac03c278c86009131fdfc97726902d484e29
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2017-05-29 16:19:38 -0600

    adding some scaffolding

---
+++ src/Engine.ts
@@ -0,0 +1,18 @@
+import { StateInfo } from './StateInfo';
+import { ResourceManager } from './Resources/ResourceManager';
+import { HarvesterManager } from './Harvesters/HarvesterManager';
+import { UpgradeManager } from './Upgrades/UpgradeManager';
+
+export class Engine{
+	StateInfo : StateInfo;
+	ResourceManager: ResourceManager;
+	HarvesterManager: HarvesterManager;
+	UpgradeManager: UpgradeManager;
+	
+	constructor() {
+		
+	}
+
+
+	
+}

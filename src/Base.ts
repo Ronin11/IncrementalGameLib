b709644ac96b68cbd4d242356a31e932ff37a3e2
Ronin11 <nate@raddd.com>
2017-06-12 17:38:28 -0600

    WIP: package.json cleanup

--- src/Base.ts
+++ src/Base.ts
@@ -1,20 +1,21 @@
 export abstract class Base {
-    protected count: number;    constructor(readonly name: string) {
-        this.count = 0;
-    }
-    getName() {
-        return this.name;
-    }
-    getCount() {
-        return this.count;
-    }
-    seCount(newCount: number) {
-        this.count = newCount;
-    }
-    incremntCount(amount = 1) {
-        this.count += amount;
-    }
-    decrementCunt(amount = 1) {
-        this.count -= amount;
-    }
+	protected count: number;
+	constructor(readonly name: string) {
+		this.count = 0;
+	}
+	getName() {
+		return this.name;
+	}
+	getCount() {
+		return this.count;
+	}
+	seCount(newCount: number) {
+		this.count = newCount;
+	}
+	incremntCount(amount = 1) {
+		this.count += amount;
+	}
+	decrementCunt(amount = 1) {
+		this.count -= amount;
+	}
 }
--- src/Harvester.ts
+++ src/Harvester.ts
@@ -1,20 +1,21 @@
 import { Base } from './Base'
 
 export class Harvester extends Base {
-    private multiplier: number;    constructor(name: string, public cost: Function, public update: Function) {
-        super(name);
-        this.multiplier = 1;
-    }
-    getMultiplier() {
-        return this.multiplier;
-    }
-    etMultiplier(newMultiplier: number) {
-        this.multiplier = newMultiplier;
-    }
-    incrmentMultiplier(amount = 1) {
-        this.multiplier += amount;
-    }
-    decremenMultiplier(amount = 1) {
-        this.multiplier -= amount;
-    }
+	private multiplier: number;
+	constructor(name: string, public cost: Function, public update: Function) {
+		super(name);
+		this.multiplier = 1;
+	}
+	getMultiplier() {
+		return this.multiplier;
+	}
+	etMultiplier(newMultiplier: number) {
+		this.multiplier = newMultiplier;
+	}
+	incrmentMultiplier(amount = 1) {
+		this.multiplier += amount;
+	}
+	decremenMultiplier(amount = 1) {
+		this.multiplier -= amount;
+	}
 }
